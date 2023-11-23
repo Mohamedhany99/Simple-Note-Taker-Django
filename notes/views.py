fe941f9ee521eecff24c9362c865086b367fd6d3
Mohamed Hany <Mohamed.hany182@outlook.com>
2023-11-23 16:35:05 +0200

    adding CRUD views

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import NoteSerializer
+from .models import Note
 
 # Create your views here.
+
+
+class ListNoteView(generics.ListAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+class AddNoteView(generics.CreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+
+class UpdateNoteView(generics.RetrieveUpdateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+
+class DeleteNoteView(generics.DestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
